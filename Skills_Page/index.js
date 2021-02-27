8e83bd05cb332a3ad71b08f963f16be9f15a3bcd
DKCBS <70820360+DKCBS@users.noreply.github.com>
2021-02-27 17:26:06 -0500

    update

---
+++ Skills_Page/index.js
@@ -0,0 +1,30 @@
+console.log("the file is wokring correctly");
+
+/* Diego Hover*/
+function Changefontweight(){
+    var text= document.getElementById("DSB").style.fontWeight = "bolder";
+}
+function backtonormal(){
+    var text= document.getElementById("DSB").style.fontWeight = "";
+}
+/* Contact Hover*/
+function ChangeFW(){
+    var text= document.getElementById("CON").style.fontWeight = "bolder";
+}
+function BackTN(){
+    var text= document.getElementById("CON").style.fontWeight = "";
+}
+/* Skills Hover*/
+function changefontweight(){
+    var text= document.getElementById("Skills").style.fontWeight = "bolder";
+}
+function Backtonormal(){
+    var text= document.getElementById("Skills").style.fontWeight = "";
+}
+/* Resume Hover*/
+function CFW(){
+    var text= document.getElementById("Resume").style.fontWeight = "bolder";
+}
+function BTN(){
+    var text= document.getElementById("Resume").style.fontWeight = "";
+}
